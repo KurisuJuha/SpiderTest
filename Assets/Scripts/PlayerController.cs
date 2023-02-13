16f3b7bc583d1a099dc7107009f1d4e2878dd6ee
KYapp <kukiyomenaisapp@gmail.com>
2023-02-13 20:47:50 +0900

    hoge

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,15 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private new Rigidbody rigidbody;
+    [SerializeField] private float speed;
 
+    private void Update()
+    {
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        transform.rotation =
+        rigidbody.velocity = Camera.main.transform.rotation * input * speed;
+    }
 }
